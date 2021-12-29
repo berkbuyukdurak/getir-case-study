cb66a2f08f87c98b96f6f3f13d7b0a622877499a
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2021-12-29 22:44:37 +0300

    Initializing the project directory. Creating core classes.

---
+++ config/env/test.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+    app: {
+        title: 'Test Environment',
+        timeout: 3000
+    },
+    server: {
+        port: process.env.PORT || 3000,
+        hostname: process.env.HOSTNAME || 'localhost'
+    },
+    db: process.env.MONGODB_URI
+}
