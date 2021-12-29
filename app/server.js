560d9e2e902fd5210c3d81d2d165c27143de56f9
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2021-12-29 23:54:14 +0300

    process.env.PORT is changed to process.env.port

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const config = require('../config/env/' + process.env.NODE_ENV);
 const logger = require('../app/utils/logger/logger');
 
 const app = require('./app');
-const port = process.env.PORT || config.server.port;
+const port = process.env.port || config.server.port;
 app.listen(port, () => {
     logger.info(`Server is started. Listening port ${port}...`);
 })
